517a2c86429f29b559f5d7f162266021c4ac26d3
Muammer Özdemir <muammer.ozdmr490@gmail.com>
2024-02-27 00:55:15 +0300

    initialize for heroku deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app
+web: gunicorn up_play.wsgi
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-from up_play import create_app
-import os
-
-app = create_app()
-
-if __name__ == "__main__":
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=False,use_reloader=False, port=5001)
